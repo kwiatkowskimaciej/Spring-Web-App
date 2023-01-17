d19eefccba4285435371d7a297769e0381a8aed1
kbabik <01169266@pw.edu.pl>
2023-01-17 20:08:20 +0100

    Literówka: w SERWISY klucz obcy odnoszący się do telefonów ma nazwę nr_zbiorow_telefonow, nie nr_zbioru telefonow, pomimo że tak nazywa się on we wszystkich innych tabelach

--- src/main/java/com/example/servingwebcontent/database/SerwisyDAO.java
+++ src/main/java/com/example/servingwebcontent/database/SerwisyDAO.java
@@ -19,24 +19,24 @@ public class SerwisyDAO {
     private JdbcTemplate jdbcTemplate;
 
     public List<Serwis> findAll(){
-        String sql = "SELECT * from SERWISY s LEFT JOIN ADRESY a ON s.nr_adresu = a.nr_adresu LEFT JOIN TELEFONY t ON s.nr_zbioru_telefonow = t.nr_zbioru_telefonow";
+        String sql = "SELECT * from SERWISY s LEFT JOIN ADRESY a ON s.nr_adresu = a.nr_adresu LEFT JOIN TELEFONY t ON s.nr_zbiorow_telefonow = t.nr_zbioru_telefonow";
         return jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Serwis.class));
     }
     public void save(Serwis serwis) {
         SimpleJdbcInsert insertActor = new SimpleJdbcInsert(jdbcTemplate);
-        insertActor.withTableName("serwisy").usingColumns("nr_serwisu", "nazwa", "liczba_stanowisk", "nr_salonu", "nr_adresu", "nr_zbioru_telefonow");
+        insertActor.withTableName("serwisy").usingColumns("nr_serwisu", "nazwa", "liczba_stanowisk", "nr_salonu", "nr_adresu", "nr_zbiorow_telefonow");
 
         BeanPropertySqlParameterSource param = new BeanPropertySqlParameterSource(serwis);
         insertActor.execute(param);
     }
     public Serwis get(int id) {
         Object[] args = {id};
-        String sql = "SELECT * FROM SERWISY s LEFT JOIN ADRESY a ON s.nr_adresu = a.nr_adresu LEFT JOIN TELEFONY t ON s.nr_zbioru_telefonow = t.nr_zbioru_telefonow WHERE nr_serwisu = " + args[0];
+        String sql = "SELECT * FROM SERWISY s LEFT JOIN ADRESY a ON s.nr_adresu = a.nr_adresu LEFT JOIN TELEFONY t ON s.nr_zbioru_telefonow = t.nr_zbiorow_telefonow WHERE nr_serwisu = " + args[0];
         Serwis serwis = jdbcTemplate.queryForObject(sql, BeanPropertyRowMapper.newInstance(Serwis.class));
         return serwis;
     }
     public void update(Serwis serwis) {
-        String sql = "UPDATE SERWISY SET nr_serwisu=:nr_serwisu, nazwa=:nazwa, liczba_stanowisk=:liczba_stanowisk, nr_salonu=:nr_salonu, nr_adresu=:nr_adresu, nr_zbioru_telefonow=:nr_zbioru_telefonow";
+        String sql = "UPDATE SERWISY SET nr_serwisu=:nr_serwisu, nazwa=:nazwa, liczba_stanowisk=:liczba_stanowisk, nr_salonu=:nr_salonu, nr_adresu=:nr_adresu, nr_zbiorow_telefonow=:nr_zbiorow_telefonow";
         BeanPropertySqlParameterSource param = new BeanPropertySqlParameterSource(serwis);
         NamedParameterJdbcTemplate template = new NamedParameterJdbcTemplate(jdbcTemplate);
 
